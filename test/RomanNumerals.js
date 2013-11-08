a58f86886340ee5a239589d41ffb057cbe4c0b07
Ben Flowers <ben.j.flowers@gmail.com>
2013-11-08 11:48:56 +0100

    removing redundant X

--- test/RomanNumerals.js
+++ test/RomanNumerals.js
@@ -30,7 +30,6 @@ describe('When I want to convert arabic numbers to roman numerals',function(){
 
 var RomanNumeralGenerator = function(){
 	var ROMAN_NUMERALS = [
-		{arabic: 20, roman: 'XX'},
 		{arabic: 10, roman: 'X'},
 		{arabic: 5, roman: 'V'},
 		{arabic: 1, roman: 'I'},
