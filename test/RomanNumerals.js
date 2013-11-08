788c563c92ad39e672d54c260ed44dfa55dc7e62
Ben Flowers <ben.j.flowers@gmail.com>
2013-11-08 11:16:10 +0100

    first number

--- test/RomanNumerals.js
+++ test/RomanNumerals.js
@@ -1,2 +1,19 @@
 var assert = require('assert');
 
+
+
+describe('When I want to convert arabic numbers to roman numerals',function(){
+	it('and that number is 1',function(){
+		var romanNumeralGenerator = new RomanNumeralGenerator(),
+			romanNumeral = romanNumeralGenerator.generate(1);
+		assert.equal(romanNumeral,'I');
+	});
+});
+
+var RomanNumeralGenerator = function(){
+	return{
+		generate: function(){
+			return 'I';
+		}
+	};
+};
